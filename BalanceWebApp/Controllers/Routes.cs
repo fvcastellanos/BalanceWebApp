f17e8019611bf8994804dfbed2398470cc83c5e3
Francisco Castellanos <fvcastellanos@gmail.com>
2017-12-30 14:58:06 -0600

    add transaction type crud

---
+++ BalanceWebApp/Controllers/Routes.cs
@@ -0,0 +1,17 @@
+﻿namespace BalanceWebApp.Controllers
+{
+    public class Routes
+    {
+        public const string Root = "balance";
+        
+        // Common routes
+        public const string New = "new";
+        
+        // Controller routes
+        public const string AccountTypes = "/account-types";
+        public const string Providers = "/providers";
+        public const string ScheduledTransactions = "/scheduled-transactions";
+        public const string Transactions = "/transactions";
+        public const string TransactionTypes = "/transaction-types";
+    }
+}
---
+++ BalanceWebApp/Model/Views/Option.cs
@@ -0,0 +1,19 @@
+﻿
+namespace BalanceWebApp.Model.Views
+{
+    public class Option
+    {
+        public string Value { get; set; }
+        public string Text { get; set; }
+
+        public Option()
+        {
+        }
+
+        public Option(string value, string text)
+        {
+            Value = value;
+            Text = text;
+        }
+    }
+}
