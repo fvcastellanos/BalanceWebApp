6d069e54f73aee5fe2ce27f1cfe08abc3cf1fffc
Francisco Castellanos <fvcastellanos@gmail.com>
2019-01-12 14:46:19 -0600

    add initial account service tests

--- BalanceWebApp.Tests/Services/AccountServiceTest.cs
+++ BalanceWebApp.Tests/Services/AccountServiceTest.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections.Generic;
 using BalanceWebApp.Model.Dao;
 using BalanceWebApp.Model.Domain;
 using BalanceWebApp.Services;
+using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
 
@@ -9,28 +11,68 @@ namespace BalanceWebApp.Tests.Services
 {
     public class AccountServiceTest
     {
+        private AccountService _accountService;
 
-        private AccountService accountService;
-
-        private Mock<IAccountDao> accountDaoMock;
-
+        private Mock<IAccountDao> _accountDao;
+        
         [SetUp]
         public void SetUp()
         {
-            accountDaoMock = new Mock<IAccountDao>();
-
-            accountService = new AccountService(null, accountDaoMock.Object, null, null);
+            _accountDao = new Mock<IAccountDao>();
+            var logger = new Logger<AccountService>(new LoggerFactory());
+            
+            _accountService = new AccountService(logger, _accountDao.Object, null, null);
         }
 
         [Test]
-        public void Testito()
+        public void TestGetAll()
         {
-            accountDaoMock.Setup(x => x.GetAll())
-                .Returns(new List<Account>());
+            _accountDao.Setup(dao => dao.GetAll())
+                .Returns(BuildAccountList);
 
-            var accounts = accountService.GetAll();
+            var result = _accountService.GetAll();
+            
+            Assert.True(result.IsSuccess());
+            Assert.NotNull(result.GetPayload());
+            
+            _accountDao.Verify(dao => dao.GetAll());
+        }
 
-            Assert.IsNotNull(accounts);
+        [Test]
+        public void TestGetAllThrowsException()
+        {
+            _accountDao.Setup(dao => dao.GetAll())
+                .Throws(new Exception("expected exception"));
+
+            var result = _accountService.GetAll();
+            
+            Assert.True(result.HasErrors());
+            Assert.AreEqual("Can't get accounts", result.GetFailure());
+            
+            _accountDao.Verify(dao => dao.GetAll());
+        }
+
+        private static ICollection<Account> BuildAccountList()
+        {
+            var list = new List<Account> { BuildAccount() };
+
+            return list;
+        }
+
+        private static Account BuildAccount()
+        {
+            return new Account()
+            {
+                AccountNumber = "123",
+                AccountType = "type",
+                AccountTypeId = 0,
+                Balance = 100,
+                Id = 0,
+                Name = "name",
+                Provider = "provider",
+                ProviderCountry = "GT",
+                ProviderId = 0
+            };
         }
     }
 }
